bd3b58a46ea10a4f7705148236223a83b0b72a26
NicholasGati <ngati016@hotmail.com>
2017-01-25 17:46:38 -0500

    changed title

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Calm Journey - The Collaborative Whiteboard' });
+  res.render('index', { title: 'Collaborative Whiteboard' });
 });
 
 module.exports = router;
